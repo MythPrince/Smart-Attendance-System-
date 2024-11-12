e940b8391928b2f02251c21fef6a0256081caf51
unknown <princekewatkar7@gmail.com>
2024-11-12 12:26:01 +0530

    Old Script Only

--- oldCode/recorded2.py
+++ oldCode/recorded2.py
@@ -4,28 +4,26 @@ import numpy as np
 import pandas as pd
 import os
 def func1():
-    # Load the video file
+ 
     video_path = "videos/vid1.mp4"
     video_capture = cv2.VideoCapture(video_path)
 
 
 
-    # Step 1: Load the training images and learn how to recognize them
+ 
     known_face_encodings = []
     known_face_names = []
 
-    # Directory where your training images are stored
+
     known_people_dir = 'images'
 
-    # Load each image in the training directory
+
     for image_name in os.listdir(known_people_dir):
         image_path = os.path.join(known_people_dir, image_name)
-        # Load the image using face_recognition
+     
         image = face_recognition.load_image_file(image_path)
-        # Get the face encodings for the first face in the image
         encoding = face_recognition.face_encodings(image)[0]
         known_face_encodings.append(encoding)
-        # Extract the name from the image filename (assumes format 'name.jpg')
         name = os.path.splitext(image_name)[0]
         known_face_names.append(name)
 
@@ -39,11 +37,9 @@ def func1():
 
     recognized_faces = []
     while video_capture.isOpened():
-        # Capture frame-by-frame
         ret, frame = video_capture.read()
 
         if not ret:
-            # print("not ret")
             break
 
         rgb_frame = np.array(frame[:, :, ::-1])
@@ -83,16 +79,12 @@ def func1():
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break  
 
-    # Release the video capture and video writer objects
     video_capture.release()
-    # out.release()
 
-    # Close all OpenCV windows
     cv2.destroyAllWindows()
 
     recognized_faces = list(set(recognized_faces))
 
-    # Save the names to a CSV file
     df = pd.DataFrame(recognized_faces, columns=['Name'])
     df.to_csv("output.csv", index=False)
 
